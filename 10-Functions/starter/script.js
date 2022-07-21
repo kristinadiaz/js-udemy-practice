715195c1e3eefb8d03d0b389ec28be06a35583c0
Kristina Diaz <kristina.diaz84@gmail.com>
2022-07-21 11:48:21 -0600

    added challenges script to section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,64 @@
 'use strict';
+
+/*
+// Default Parameters
+const bookings = [];
+
+const createBooking = function(
+    flightNum, 
+    numPassengers = 1, 
+    price = 199 * numPassengers
+) {
+    // ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+// Passing Arguments
+const flight = 'LH234';
+const kristina = {
+    name: 'Kristina Diaz',
+    passport: 6475027524
+};
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Ms. ' + passenger.name;
+
+    if(passenger.passport === 6475027524) {
+        console.log('Check In')
+    } else {
+        console.log('Wrong passport!')
+    }
+};
+
+// checkIn(flight, kristina);
+// console.log(flight);
+// console.log(kristina);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = kristina;
+
+const newPassport = function(person) {
+    person.passport = Math.floor(Math.random() * 10000000000);
+};
+
+newPassport(kristina);
+checkIn(flight, kristina);
+*/
+
